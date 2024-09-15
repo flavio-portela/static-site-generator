cf37c49f9a237feac31a218a347dfd807e19e00b
Flavio Portela <portela828@gmail.com>
2024-09-14 19:45:12 -0700

    more tests

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -8,3 +8,11 @@ class TestLeafNode(unittest.TestCase):
         self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+    def test_to_html_no_value(self):
+        node = LeafNode("p")
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(value="just text")
+        self.assertEqual(node.to_html(), "just text")
